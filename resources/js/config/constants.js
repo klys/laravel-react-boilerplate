ac67a71578b707a6fc7c9f49957346078debac8b
Junior Jimenez <klys2007@gmail.com>
2022-10-01 21:43:01 -0500

    Migration fixes

--- resources/js/config/constants.js
+++ resources/js/config/constants.js
@@ -1,5 +1,5 @@
 const constants = {
-  HOST_URL: 'https://laravel-react-boilerplate.herokuapp.com/api',
+  HOST_URL: 'http://127.0.0.1/api',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
